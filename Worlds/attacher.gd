44150e4b62bead522dc7f68fcf2e47e011ef307e
piotrus <piotruswatson@gmail.com>
2024-03-28 19:27:43 +0000

    added jolt, activated jolt. fixed bug where camera doesn't do things properly

--- Worlds/attacher.gd
+++ Worlds/attacher.gd
@@ -2,19 +2,34 @@ extends Generic6DOFJoint3D
 class_name Attacher
 
 var rigidbody: RigidBody3D
-
+var layer
 func _setup(body: RigidBody3D):
 	rigidbody = body
-
+	layer = rigidbody.collision_layer
+	toggle_angular_limit(false)
+	toggle_linear_limit(false)
 
 func attach(other: RigidBody3D):
-	#rigidbody.global_position = other.global_position
-	#rigidbody.rotate_x(90)
 	node_b = other.get_path()
-	rigidbody.global_position = other.global_position
+	toggle_linear_spring(true)
+	toggle_linear_limit(true)
+	#await get_tree().create_timer(0.05).timeout
 	
 	
+func toggle_linear_limit(is_on: bool):
+	set("linear_limit_x/enabled", is_on)
+	set("linear_limit_y/enabled", is_on)
+	set("linear_limit_z/enabled", is_on)
 	
+func toggle_angular_limit(is_on: bool):
+	set("angular_limit_x/enabled", is_on)
+	set("angular_limit_y/enabled", is_on)
+	set("angular_limit_z/enabled", is_on)
+	
+func toggle_linear_spring(is_on: bool):
+	set("linear_spring_x/enabled", is_on)
+	set("linear_spring_y/enabled", is_on)
+	set("linear_spring_z/enabled", is_on)
 
 func detach():
 	node_b = NodePath()
